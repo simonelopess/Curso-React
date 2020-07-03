20c577a501939c2aab11a28aa6a216ae8e22107e
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-07-03 16:20:34 -0300

    Correção na configuração da navegação

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -56,7 +56,7 @@ export default class Main extends Component{
                     <article key={product._id}>
                         <strong>{product.title}</strong>
                         <p>{product.description}</p>
-                        <Link to={`/products/product._id`}>Acessar</Link>
+                        <Link to={`/products/${product._id}`}>Acessar</Link>
                     </article>
                 ))}
                 <div className="actions">
